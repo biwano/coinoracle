520ae5cf9e822809805d5a73411fd885525731e7
biwano <biwano@gmail.com>
2025-02-14 17:12:27 +0100

    Handle errors

--- src/stats.py
+++ src/stats.py
@@ -4,8 +4,11 @@ import requests
 @click.command()
 def stats():
     res = requests.get("http://ftxbot.ilponse.com/api/bots/btc/stats")
-    data = res.json()["result"]["btc"]
-    click.echo(f"|\tdirection\t|\tprice\t|")
-    click.echo(f"|\t{round(data['direction'])}\t\t|\t{round(data['price'])}\t|")
+    try:
+        data = res.json()["result"]["btc"]
+        click.echo(f"|\tdirection\t|\tprice\t|")
+        click.echo(f"|\t{round(data['direction'])}\t\t|\t{round(data['price'])}\t|")
+    except:
+        click.echo(f"Error status: {res.status_code}")
 
 
